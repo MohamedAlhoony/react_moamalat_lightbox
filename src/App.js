bffd94e1fff81a8b452d9015dda46010f4f76245
Your Name <mohammedfarhat1995@gmail.com>
2024-10-08 14:47:48 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
-
+import logo from "./logo.svg";
+import "./App.css";
+import Moamalat from "reactjs-moamalat";
 function App() {
+  const payButtonStyle = {
+    color: "#000000",
+    backgroundColor: "#CCCCCC",
+    borderRadius: "10px",
+  };
+  const date = Date.now();
   return (
     <div className="App">
-      <header className="App-header">
+      <Moamalat
+        Amount={"1000"}
+        TID={"99179395"}
+        MID={"10081014649"}
+        MerchantReference="eeeeeeeeeeeefasdfetter"
+        secretKey={
+          "39636630633731362D663963322D346362642D386531662D633963303432353936373431"
+        }
+        dateTime={date}
+        errorCallback={(error) => {
+          console.log(error);
+        }}
+        completePaymentCallback={() => {}}
+        errorPaymentCallback={(error) => {
+          console.log(error);
+        }}
+        cancelPaymentCallback={() => {}}
+        env={"staging"}
+        payButtonStyle={payButtonStyle}
+      />
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -17,7 +43,7 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
     </div>
   );
 }
